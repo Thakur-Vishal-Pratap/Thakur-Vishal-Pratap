2e6160bd1b2aa857c8ea231ebdef6fff3a5a1068
Thakur-Vishal-Pratap <vishalpratap844@gmail.com>
2022-04-03 22:54:11 +0530

    tree changes

--- Node/FileOrganizer/commands/tree.js
+++ Node/FileOrganizer/commands/tree.js
@@ -3,27 +3,38 @@ const path = require ("path");
 
 function tree(srcPath){
     if(srcPath==undefined){
-        srcPath=process.cwd();
+        console.log("Please Enter a Valid Path");
+        return;
     }
-    // let allFiles = fs.readdirSync(srcPath);
-    printAllFileName(srcPath);
-
+    let doesExist = fs.existsSync(srcPath);
+    if(doesExist){
+        printAllFileName(srcPath , " ");
+    }
 }
 
-function printAllFileName(fileNow){
+function printAllFileName(fileNow , indent){
     let isFile = fs.lstatSync(fileNow).isFile();
     // console.log();
-    if(isFile == false){
-        console.log(path.basename(fileNow) + "\n");
-        let allFiles = fs.readdirSync(fileNow);
-        for(let i=0 ; i<allFiles.length ; i++){
-            let newFile = path.join(fileNow , allFiles[i])
-            printAllFileName(newFile);
-        }
-    }else{
-        console.log(path.basename(fileNow) + "\n");
+
+    if(isFile){
+        let fileName = path.basename(fileNow);
+        console.log(indent + "|---" + fileName);
+        return;
     }
+    
+    let dirName = path.basename(fileNow);
+    console.log(indent + "'---" + dirName);
+    let allFiles = fs.readdirSync(fileNow);
+    for(let i=0 ; i<allFiles.length ; i++){
+        let newFile = path.join(fileNow , allFiles[i])
+        printAllFileName(newFile , indent+"\t");
+    }
+    
 }
 
-let srcPath = "C:/Users/Asus/Desktop/Web_Dev/Node/FileOrganizer/downloads";
-tree(srcPath);
+// let srcPath = "C:/Users/Asus/Desktop/Web_Dev/Node/FileOrganizer/downloads";
+// tree(srcPath);
+
+module.exports={
+    tree:tree
+}
--- Node/FileOrganizer/main.js
+++ Node/FileOrganizer/main.js
@@ -1,12 +1,14 @@
 let inputArr = process.argv.slice(2);
 let helpFunc = require("./commands/help");
 let organizeFunc = require("./commands/organize");
+let treeFunc = require("./commands/tree")
 let command = inputArr[0];
 let path = inputArr[1];     
 switch (command){
     case "tree":
         //call tree function
-        console.log("This is tree function called and executed");
+        // console.log("This is tree function called and executed");
+        treeFunc.tree(path);
         break;
     case "organize":
         organizeFunc.organize(path);
